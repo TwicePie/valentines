8349923accdb423298dd0e961735636aeb5227d0
TwicePie <kknurarief@gmail.com>
2025-02-09 17:12:41 +0700

    initialize React + TypeScript + Vite project with Tailwind CSS and DaisyUI

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <Link to="/" className="btn btn-ghost text-xl">Valentine's</Link>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold">About Us</h1>
+      <p className="mt-4">This is the about page of our Valentine's website.</p>
+    </div>
+  );
+};
+
+export default About;
