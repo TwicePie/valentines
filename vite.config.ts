59945b7318b12898186757cb1d8ac02ae94ec5de
TwicePie <kknurarief@gmail.com>
2025-02-10 18:37:46 +0700

    update Vite configuration to set base path to root

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/valentines/', // Add this line - use your repository name
+  base: '/' // Make sure base is set to root
 })
