2677cdc7b421954b94dc73e6773d7558fa0f4fc6
TwicePie <kknurarief@gmail.com>
2025-02-10 19:43:48 +0700

    update routing to set Home component path to '/valentines/'

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <Router>
       <div className="min-h-screen">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/valentines/" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </div>
