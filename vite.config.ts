1e305a2dc6969e1ee320a714b096816935830836
TwicePie <kknurarief@gmail.com>
2025-02-10 19:45:10 +0700

    fix: correct base path in Vite configuration to include trailing slash

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/valentines' // Make sure base is set to root
+  base: '/valentines/' // Make sure base is set to root
 })
